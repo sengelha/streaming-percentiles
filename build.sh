2a683e9d0eeffa0b0f49f29d84bce31facfb888d
Steven Engelhardt <sengelha@gmail.com>
2018-02-26 17:38:15 -0600

    Add build scripts which work on Mac OSX (and presumably Linux)

---
+++ build.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+set -e
+
+usage()
+{
+    echo "Usage: $0 [options]"
+    echo
+    echo "Options:"
+    echo "  -c, --clean          perform a clean build"
+    echo "  -d, --debug          build a debug build (default)"
+    echo "  -h, --help           print help and exit"
+    echo "  -r, --release        build a release build"
+    exit 1
+}
+
+CLEAN_BUILD=0
+BUILD_TYPE=Debug
+
+while :; do
+    if [ $# -le 0 ]; then
+        break
+    fi
+
+    lowerI="$(echo $1 | awk '{print tolower($0)}')"
+    case $lowerI in
+        -c|--clean)
+            CLEAN_BUILD=1
+            ;;
+
+        -d|--debug)
+            BUILD_TYPE=Debug
+            ;;
+
+        -\?|-h|--help)
+            usage
+            exit 1
+            ;;
+
+        -r|--release)
+            BUILD_TYPE=RelWithDebInfo
+            ;;
+
+        *)
+            echo "Unrecognized option $1"
+            exit 1
+            ;;
+    esac
+
+    shift
+done
+
+echo "Building streaming-percentiles with the following settings:"
+echo "  Build type: ${BUILD_TYPE}"
+echo "  Clean build: ${CLEAN_BUILD}"
+echo
+
+if [ $CLEAN_BUILD -eq 1 ]; then
+  rm -rf build
+fi
+if [ ! -d build ]; then
+  mkdir build
+fi
+cd build
+cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+cmake --build .
+env CTEST_OUTPUT_ON_FAILURE=1 ctest
--- src/gk.cpp
+++ src/gk.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cmath>
 #include <iostream>
 #include "gk.hpp"
 
