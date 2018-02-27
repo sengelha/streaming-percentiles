8419eb19ea42ab3a30b6f16dd18bc988075cabf5
Steven Engelhardt <sengelha@gmail.com>
2018-02-26 20:46:17 -0600

    Replace build.bat with version from deeznes

--- build.bat
+++ build.bat
@@ -1,3 +1,10 @@
 @echo off
 
-cmd /c build2.bat
+set BUILD_TYPE="Visual Studio 14 2015 Win64"
+set BUILD_CONFIG=Debug
+
+if not exist target mkdir target
+
+cmd /c "cd target & cmake -G %BUILD_TYPE% --config %BUILD_CONFIG% .." || exit /b 1
+cmd /c "cd target & cmake --build . --config %BUILD_CONFIG%" || exit /b 1
+cmd /c "set CTEST_OUTPUT_ON_FAILURE=TRUE & cd target & ctest -C %BUILD_CONFIG%" || exit /b 1
--- build2.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set VCVARS_BAT=C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat
-
-call "%VCVARS_BAT%" amd64
-if not exist build mkdir build
-cd build
-cmake -G "NMake Makefiles" CMAKE_BUILD_TYPE:Release ..
-nmake
