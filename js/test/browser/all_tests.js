6ca0972f44e83c19f723a7ad0cdaa62db4e8a61a
Steven Engelhardt <sengelha@gmail.com>
2018-04-14 11:47:30 -0500

    Add require.js test case

--- js/test/browser/all_tests.js
+++ js/test/browser/all_tests.js
@@ -28,5 +28,20 @@ module.exports = {
             .pause(1000)
             .assert.containsText('#result', '0.4948999999999618')
             .end();
+    },
+    'RequireJS Test' : function (client) {
+        client
+            .url('http://localhost:8080/requirejs_test.html')
+            .getLogTypes(function(result) {
+                console.log(result);
+            })
+            .getLog('browser', function(result) {
+                console.log(result);
+            })
+            .waitForElementVisible('body', 1000)
+            .assert.title('RequireJS Test')
+            .pause(1000)
+            .assert.containsText('#result', '0.4948999999999618')
+            .end();        
     }
 };
