04bde863f3e06351d9ba4bb14d0231224f4b25f3
Steven Engelhardt <sengelha@gmail.com>
2018-04-14 10:26:43 -0500

    Add CKMS_HBQ test to browser tests

--- js/test/browser/all_tests.js
+++ js/test/browser/all_tests.js
@@ -1,17 +1,32 @@
 module.exports = {
+    'CKMS_HBQ Sanity': function(client) {
+        client
+            .url('http://localhost:8080/ckms_hbq_sanity.html')
+            .getLogTypes(function(result) {
+                console.log(result);
+            })
+            .getLog('browser', function(result) {
+                console.log(result);
+            })
+            .waitForElementVisible('body', 1000)
+            .assert.title('CKMS_HBQ Sanity')
+            .pause(1000)
+            .assert.containsText('#result', '0.4997999999999613')
+            .end();
+    },
     'GK Sanity' : function (client) {
-	client
-	    .url('http://localhost:8080/gk_sanity.html')
-	    .getLogTypes(function(result) {
-		console.log(result);
-	    })
-	    .getLog('browser', function(result) {
-		console.log(result);
-	    })
-	    .waitForElementVisible('body', 1000)
-	    .assert.title('GK Sanity')
-	    .pause(1000)
-	    .assert.containsText('#result', '0.4948999999999618')
-	    .end();
+        client
+            .url('http://localhost:8080/gk_sanity.html')
+            .getLogTypes(function(result) {
+                console.log(result);
+            })
+            .getLog('browser', function(result) {
+                console.log(result);
+            })
+            .waitForElementVisible('body', 1000)
+            .assert.title('GK Sanity')
+            .pause(1000)
+            .assert.containsText('#result', '0.4948999999999618')
+            .end();
     }
 };
