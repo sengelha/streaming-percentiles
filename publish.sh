a5b94fc78583058f23a6e1d481307642947e94a2
Steven Engelhardt <sengelha@gmail.com>
2018-04-20 19:14:46 -0500

    Update publish

--- publish.sh
+++ publish.sh
@@ -4,8 +4,35 @@
 
 set -e
 
-# TODO: Grab source code by tag
+usage()
+{
+    echo "Usage: $0 TAG_NAME"
+    exit 1
+}
+
+if [ $# -ne 1 ]; then
+  usage
+fi
+
+TAG_NAME=$1
+
+echo Deleting target/publish_root...
+rm -rf target/publish_root
+echo Creating target/publish_root...
+mkdir -p target/publish_root
+echo Cloning streaming-percentiles-cpp.git...
+cd target/publish_root
+git clone https://github.com/sengelha/streaming-percentiles-cpp.git
+cd streaming-percentiles-cpp
+echo Checking out tag $TAG_NAME...
+git checkout tags/$TAG_NAME
+echo Building package...
+./build.sh --release --clean
+
 # TODO: Create github release if it doesn't exist
 # TODO: Copy built binary to github release page
-# TODO: Run npm publish
 # TODO: Copy JS to sengelha.github.io
+# TODO: Run npm publish
+#  echo Publishing to NPM...
+#  -  ( cd js/src/npm_package && npm publish )
+
