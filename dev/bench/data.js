5fd8b8e27cd3e182855d36f1dc5bd18b283ef6be
github-action-benchmark <github@users.noreply.github.com>
2021-11-24 06:03:15 +0000

    add Benchmark (googlecpp) benchmark result for 8f9210d8c37eb0b8d1a283a0a88cbdf11e61f4c3

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1637733795287,
+  "repoUrl": "https://github.com/sengelha/streaming-percentiles",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "sengelha@gmail.com",
+            "name": "Steven Engelhardt",
+            "username": "sengelha"
+          },
+          "committer": {
+            "email": "sengelha@gmail.com",
+            "name": "Steven Engelhardt",
+            "username": "sengelha"
+          },
+          "distinct": true,
+          "id": "8f9210d8c37eb0b8d1a283a0a88cbdf11e61f4c3",
+          "message": "Save work",
+          "timestamp": "2021-11-24T00:02:36-06:00",
+          "tree_id": "68a101a616022e0271c54bc5f8f002a3dd6b455c",
+          "url": "https://github.com/sengelha/streaming-percentiles/commit/8f9210d8c37eb0b8d1a283a0a88cbdf11e61f4c3"
+        },
+        "date": 1637733794816,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_SomeFunction",
+            "value": 6701.6372686525165,
+            "unit": "ns/iter",
+            "extra": "iterations: 104136\ncpu: 6699.924704232925 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
