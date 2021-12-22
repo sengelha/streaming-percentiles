0aa8a25cb09bfd1e958808a3be9ac587d6618e0f
github-action-benchmark <github@users.noreply.github.com>
2021-12-22 20:56:53 +0000

    add Benchmark (googlecpp) benchmark result for 905e9b8991c908ee08eb15c16dbd93b6c0a2cbfa

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1640204106276,
+  "lastUpdate": 1640206613035,
   "repoUrl": "https://github.com/sengelha/streaming-percentiles",
   "entries": {
     "Benchmark": [
@@ -156,6 +156,84 @@ window.BENCHMARK_DATA = {
             "value": 531089782.71484375,
             "unit": "ns/iter",
             "extra": "iterations: 1\ncpu: 531057399.9999946 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sengelha@gmail.com",
+            "name": "Steven Engelhardt",
+            "username": "sengelha"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "905e9b8991c908ee08eb15c16dbd93b6c0a2cbfa",
+          "message": "Merge pull request #55 from sengelha/optimize_gk\n\nnew: Significantly speed up gk for small epsilon",
+          "timestamp": "2021-12-22T14:53:26-06:00",
+          "tree_id": "12c68f3bf23cfbae2dedceb801e57ccd87c9857e",
+          "url": "https://github.com/sengelha/streaming-percentiles/commit/905e9b8991c908ee08eb15c16dbd93b6c0a2cbfa"
+        },
+        "date": 1640206612177,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "ckms_hbq e=0.1 n=1000000 sorted",
+            "value": 130252313.6138916,
+            "unit": "ns/iter",
+            "extra": "iterations: 5\ncpu: 130225920 ns\nthreads: 1"
+          },
+          {
+            "name": "ckms_hbq e=0.01 n=1000000 sorted",
+            "value": 564686536.7889404,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 564658100 ns\nthreads: 1"
+          },
+          {
+            "name": "ckms_hbq e=0.001 n=1000000 sorted",
+            "value": 4121505975.7232666,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 4121231799.9999995 ns\nthreads: 1"
+          },
+          {
+            "name": "ckms_uq e=0.1 n=1000000 sorted",
+            "value": 108129779.4977824,
+            "unit": "ns/iter",
+            "extra": "iterations: 6\ncpu: 108119299.99999996 ns\nthreads: 1"
+          },
+          {
+            "name": "ckms_uq e=0.01 n=1000000 sorted",
+            "value": 384816408.15734863,
+            "unit": "ns/iter",
+            "extra": "iterations: 2\ncpu: 384803300.0000002 ns\nthreads: 1"
+          },
+          {
+            "name": "ckms_uq e=0.001 n=1000000 sorted",
+            "value": 3118571996.688843,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 3106635000.0000005 ns\nthreads: 1"
+          },
+          {
+            "name": "gk e=0.1 n=1000000 sorted",
+            "value": 18680848.573383532,
+            "unit": "ns/iter",
+            "extra": "iterations: 38\ncpu: 18675942.10526318 ns\nthreads: 1"
+          },
+          {
+            "name": "gk e=0.01 n=1000000 sorted",
+            "value": 77588134.34176975,
+            "unit": "ns/iter",
+            "extra": "iterations: 9\ncpu: 77580622.2222223 ns\nthreads: 1"
+          },
+          {
+            "name": "gk e=0.001 n=1000000 sorted",
+            "value": 540161848.0682373,
+            "unit": "ns/iter",
+            "extra": "iterations: 1\ncpu: 540114400.0000002 ns\nthreads: 1"
           }
         ]
       }
