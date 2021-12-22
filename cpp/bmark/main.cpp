e2e03a31152622a9313095eba921088de5a619a0
Steven Engelhardt <sengelha@gmail.com>
2021-12-22 11:50:10 -0600

    chg: Rewrite benchmarks to be clearer about what they are testing

--- cpp/bmark/main.cpp
+++ cpp/bmark/main.cpp
@@ -4,53 +4,77 @@
 #include <stmpct/ckms_lbq.hpp>
 #include <stmpct/ckms_uq.hpp>
 
-static void BM_ckms_hbq(benchmark::State& state) {
+template <class ...Args>
+static void BM_ckms_hbq(benchmark::State& state, Args&&... args) {
+  auto tuple = std::forward_as_tuple(std::forward<Args>(args)...);
+  double epsilon = std::get<0>(tuple);
+  int n = std::get<1>(tuple);
   for (auto _ : state) {
-    double epsilon = 1/(double)state.range(0);
     stmpct::ckms_hbq<double> g(epsilon);
-    for (int i = 1; i <= state.range(1); ++i) {
+    for (int i = 1; i <= n; ++i) {
         g.insert(i);
     }
     benchmark::DoNotOptimize(g.quantile(0.95));
   }
+  state.counters["Rate"] = benchmark::Counter(n, benchmark::Counter::kIsRate);
 }
 
-static void BM_ckms_lbq(benchmark::State& state) {
+template <class ...Args>
+static void BM_ckms_lbq(benchmark::State& state, Args&&... args) {
+  auto tuple = std::forward_as_tuple(std::forward<Args>(args)...);
+  double epsilon = std::get<0>(tuple);
+  int n = std::get<1>(tuple);
   for (auto _ : state) {
-    double epsilon = 1/(double)state.range(0);
-    stmpct::ckms_hbq<double> g(epsilon);
-    for (int i = 1; i <= state.range(1); ++i) {
+    stmpct::ckms_lbq<double> g(epsilon);
+    for (int i = 1; i <= n; ++i) {
         g.insert(i);
     }
     benchmark::DoNotOptimize(g.quantile(0.95));
   }
+  state.counters["Rate"] = benchmark::Counter(n, benchmark::Counter::kIsRate);
 }
 
-static void BM_ckms_uq(benchmark::State& state) {
+template <class ...Args>
+static void BM_ckms_uq(benchmark::State& state, Args&&... args) {
+  auto tuple = std::forward_as_tuple(std::forward<Args>(args)...);
+  double epsilon = std::get<0>(tuple);
+  int n = std::get<1>(tuple);
   for (auto _ : state) {
-    double epsilon = 1/(double)state.range(0);
     stmpct::ckms_uq<double> g(epsilon);
-    for (int i = 1; i <= state.range(1); ++i) {
+    for (int i = 1; i <= n; ++i) {
         g.insert(i);
     }
     benchmark::DoNotOptimize(g.quantile(0.95));
   }
+  state.counters["Rate"] = benchmark::Counter(n, benchmark::Counter::kIsRate);
 }
 
-static void BM_gk(benchmark::State& state) {
+template <class ...Args>
+static void BM_gk(benchmark::State& state, Args&&... args) {
+  auto tuple = std::forward_as_tuple(std::forward<Args>(args)...);
+  double epsilon = std::get<0>(tuple);
+  int n = std::get<1>(tuple);
   for (auto _ : state) {
-    double epsilon = 1/(double)state.range(0);
     stmpct::gk<double> g(epsilon);
-    for (int i = 1; i <= state.range(1); ++i) {
+    for (int i = 1; i <= n; ++i) {
         g.insert(i);
     }
     benchmark::DoNotOptimize(g.quantile(0.95));
   }
+  state.counters["Rate"] = benchmark::Counter(n, benchmark::Counter::kIsRate);
 }
 
-BENCHMARK(BM_ckms_hbq)->Ranges({{8, 4096}, {512, 32768}});
-BENCHMARK(BM_ckms_lbq)->Ranges({{8, 4096}, {512, 32768}});
-BENCHMARK(BM_ckms_uq)->Ranges({{8, 4096}, {512, 32768}});
-BENCHMARK(BM_gk)->Ranges({{8, 4096}, {512, 32768}});
+BENCHMARK_CAPTURE(BM_ckms_hbq, e0_1_n1000000_sorted, 0.1, 1000000)->Name("ckms_hbq e=0.1 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_ckms_hbq, e0_01_n1000000_sorted, 0.01, 1000000)->Name("ckms_hbq e=0.01 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_ckms_hbq, e0_001_n1000000_sorted, 0.001, 1000000)->Name("ckms_hbq e=0.001 n=1000000 sorted");
+// BENCHMARK_CAPTURE(BM_ckms_lbq, e0_1_n1000000_sorted, 0.1, 1000000)->Name("ckms_lbq e=0.1 n=1000000 sorted");
+// BENCHMARK_CAPTURE(BM_ckms_lbq, e0_01_n1000000_sorted, 0.01, 1000000)->Name("ckms_lbq e=0.01 n=1000000 sorted");
+// BENCHMARK_CAPTURE(BM_ckms_lbq, e0_001_n1000000_sorted, 0.001, 1000000)->Name("ckms_lbq e=0.001 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_ckms_uq, e0_1_n1000000_sorted, 0.1, 1000000)->Name("ckms_uq e=0.1 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_ckms_uq, e0_01_n1000000_sorted, 0.01, 1000000)->Name("ckms_uq e=0.01 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_ckms_uq, e0_001_n1000000_sorted, 0.001, 1000000)->Name("ckms_uq e=0.001 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_gk, e0_1_n1000000_sorted, 0.1, 1000000)->Name("gk e=0.1 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_gk, e0_01_n1000000_sorted, 0.01, 1000000)->Name("gk e=0.01 n=1000000 sorted");
+BENCHMARK_CAPTURE(BM_gk, e0_001_n1000000_sorted, 0.001, 1000000)->Name("gk e=0.001 n=1000000 sorted");
 
 BENCHMARK_MAIN();
