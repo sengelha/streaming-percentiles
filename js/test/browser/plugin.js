20bb1634da0cab9304a814bb9cc1a762aaa90314
Steven Engelhardt <sengelha@gmail.com>
2021-12-27 20:27:56 -0600

    Save work

--- js/test/browser/plugin.js
+++ js/test/browser/plugin.js
@@ -1,6 +1,7 @@
 "use strict";
-exports.__esModule = true;
+
 var express = require("express");
+
 module.exports = function (on, config) {
     var app = express();
     app.get('/', function (req, res) {
